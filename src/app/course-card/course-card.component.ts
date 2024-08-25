04e8c424b56d5cbb01226efd2108811d08afcb34
Tobiloba <tobitobe66@gmail.com>
2024-08-25 15:15:46 +0100

    feat: Learn @empty,  and other options

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -12,7 +12,10 @@ export class CourseCardComponent implements OnInit {
   @Input()
   course: Course;
 
-  @Output('courseSelected')
+  @Input({ required: true })
+  index: number;
+
+  @Output("courseSelected")
   courseEmitter = new EventEmitter<Course>();
 
   constructor() {}
